5e6a88414679ecf4be666468ddc3a32ae623db69
alexescobar28 <alex.escobar@epn.edu.ec>
2022-02-28 23:28:58 -0500

    UN ESPACIO PARA SUBTOTAL

--- ProyectoFinal/Facturas.Designer.cs
+++ ProyectoFinal/Facturas.Designer.cs
@@ -133,9 +133,9 @@
             this.label8.AutoSize = true;
             this.label8.Location = new System.Drawing.Point(70, 322);
             this.label8.Name = "label8";
-            this.label8.Size = new System.Drawing.Size(84, 13);
+            this.label8.Size = new System.Drawing.Size(87, 13);
             this.label8.TabIndex = 7;
-            this.label8.Text = "SubtotalComida:";
+            this.label8.Text = "Subtotal Comida:";
             // 
             // label9
             // 

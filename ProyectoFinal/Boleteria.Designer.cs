9727ea9188fa9fe7c07467d8cd510c15d3399091
alexescobar28 <alex.escobar@epn.edu.ec>
2022-03-01 01:37:30 -0500

    BLOQUEAR MAXIMIZAR Y TAMAÑO DE LOS FORMS

--- ProyectoFinal/Boleteria.Designer.cs
+++ ProyectoFinal/Boleteria.Designer.cs
@@ -359,7 +359,9 @@
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.cmbPeliculas);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
             this.Name = "frmBoleteria";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Boleteria ";
--- ProyectoFinal/Comida.Designer.cs
+++ ProyectoFinal/Comida.Designer.cs
@@ -367,7 +367,9 @@
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
             this.ForeColor = System.Drawing.Color.Black;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
             this.Name = "frmComida";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Comida";
